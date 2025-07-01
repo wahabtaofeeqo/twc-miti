73ab224b21a667a06739fb877a2755cd63067ca9
Wahab Taofeek <taofeekolamilekan218@yahoo.com>
2025-07-01 12:05:55 +0100

    Fix

--- resources/js/Pages/Index.tsx
+++ resources/js/Pages/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
         <div className="max-w-7xl mx-auto h-full py-5 overlay">
            
             <div className="flex items-center p-3 h-full">
-                <div className="lg:basis-2/4">
+                <div className="lg:basis-2/4 mx-auto text-center">
                     {/* <p className="text-4xl font-black text-white">
                         MC Miti Comedy
                     </p> */}
@@ -22,25 +22,8 @@ const Index = () => {
                         "The human race has only one really effective weapon and that is laughter."
                     </p>
 
-                    <div className="mb-10 flex gap-3">
-                        <a href="">
-                            <i className="fab fa-facebook text-sky-300"></i>
-                        </a>
-                        <a href="">
-                            <i className="fab fa-instagram text-pink-500"></i>
-                        </a>
-                        <a href="">
-                            <i className="fab fa-youtube text-red-500"></i>
-                        </a>
-                    </div>
-
                     <div>
                         <Link href="/tickets" className="py-3 px-10 bg-white rounded shadow-sm">Get Your Ticket</Link>
-                    </div>
-                </div>
-                <div className="hidden lg:flex basis-2/4 items-center justify-center">
-                    <div className="rounded-full bg-white flex items-center justify-center main" style={{width: "500px", height: "500px"}}>
-                        {/* <img src="/images/bg.png" alt="BG" width="300px" height="300px" /> */}
                     </div>
                 </div>
             </div>
